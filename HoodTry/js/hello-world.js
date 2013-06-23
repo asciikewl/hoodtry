0a58a118b75a146d1d1c92169432080b469b8e50
Anton de Wet <ascii@icenium.phun.net>
2013-06-22 19:56:31 -0500

    new test list function

--- HoodTry/js/hello-world.js
+++ HoodTry/js/hello-world.js
@@ -2,6 +2,19 @@
 // Wait for PhoneGap to load
 document.addEventListener("deviceready", onDeviceReady, false);
 
+
+function testlist(
+
+{
+var txt1="<p>Text.</p>";              // Create text with HTML
+var txt2=$("<p></p>").text("Text.");  // Create text with jQuery
+var txt3=document.createElement("p");
+txt3.innerHTML="Text.";               // Create text with DOM
+$("body").append(txt1,txt2,txt3);        // Append new elements
+}
+$((document).refresh())
+    )
+
 // PhoneGap is ready
 function onDeviceReady() {
     getLocation();
