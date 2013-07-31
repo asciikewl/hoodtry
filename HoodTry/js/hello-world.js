72678af334a7c2e3ede5b41c75d64039283682ca
Anton de Wet <ascii@icenium.phun.net>
2013-07-31 13:21:31 -0500

    fix anton syntax

--- HoodTry/js/hello-world.js
+++ HoodTry/js/hello-world.js
@@ -201,9 +201,8 @@ function listintype1() {
            currentcommunity = community ;
              $("#eventcommunity").val(community._id)    ;
         });
-          
-       
-       
+        
+      });
     });
 }
 
@@ -218,11 +217,6 @@ function listcommunity2() {
       $.each(data[3].intypes, function(key, intype) { 
  
           options += '<li><a href="#reportpage"   > <img style="width: 50px; height: 50px;" src="images/redface.jpg" /> <h3> '+intype.label+'</h3><p> '+'--thing of community---'+'</p></a></li>';
-      
-
-       
-//-----------------------------------------
-      
       
       });
    
